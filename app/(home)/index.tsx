5a5e938fbc19c5014ed527a9c10bbc876ea886c7
Chan <chan@ipodclasic128gb.home>
2024-08-27 13:41:44 +0100

    added sign out button

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,14 +1,16 @@
-import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import React from "react";
+import { SignedIn, SignedOut, useUser, useClerk } from "@clerk/clerk-expo";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 
 export default function Page() {
   const { user } = useUser();
-
+  const { signOut } = useClerk();
   return (
     <View>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <Button title="Sign Out" onPress={() => signOut()} />
       </SignedIn>
       <SignedOut>
         <Link href="/sign-in">
