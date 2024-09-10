36906c7d27c1e391eef89a4ec35943b177d77228
Chan <chan@ipodclasic128gb.home>
2024-09-10 10:28:36 +0200

    adjustment

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -30,88 +30,138 @@ export default function Page() {
       </SignedIn> */}
       <SignedOut>
         <View
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            height: getScreenHeight() * 0.7,
+          style={{ flex: 6 }}
+          // style={{
+          //   justifyContent: "center",
+          //   alignItems: "center",
+          //   height: getScreenHeight() * 0.7,
 
-            width: getScreenWidth() * 0.7,
-            borderRadius: 10,
-            shadowColor: "black",
-            shadowOpacity: 50,
-            shadowRadius: 3,
-            backgroundColor: thirdColor,
-            borderColor: "grey",
-          }}
+          //   width: getScreenWidth() * 0.7,
+          //   borderRadius: 10,
+          //   shadowColor: "black",
+          //   shadowOpacity: 50,
+          //   shadowRadius: 3,
+          //   backgroundColor: thirdColor,
+          //   borderColor: "grey",
+          // }}
         >
-          <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          {/* <Text>Hello</Text> */}
+        </View>
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Link
+            href="/sign-in"
+            // href="/registration"
+            style={{
+              height: "70%",
+              width: "50%",
+            }}
           >
-            <Link
-              href="/sign-in"
+            <TouchableOpacity
               style={{
+                backgroundColor: fourthColor,
+                borderRadius: 10,
+                width: getScreenWidth() * 0.7,
                 height: "70%",
-                width: "50%",
+                justifyContent: "center",
+                alignItems: "center",
+                margin: 10,
+                borderWidth: 1,
               }}
             >
-              <TouchableOpacity
-                style={{
-                  backgroundColor: fourthColor,
-                  borderRadius: 10,
-                  width: getScreenWidth() * 0.5,
-                  height: "100%",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <View
-                  style={{
-                    width: "100%",
-                  }}
-                >
-                  <Text style={{ fontWeight: "bold", textAlign: "center" }}>
-                    Sign In
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            </Link>
-          </View>
-          <View style={{ flex: 1 }}>
-            <Link
-              href="/sign-up"
-              style={{
-                height: "70%",
-                width: "50%",
-              }}
-            >
-              <TouchableOpacity
-                style={{
-                  backgroundColor: fourthColor,
-                  borderRadius: 10,
-                  width: getScreenWidth() * 0.5,
-                  height: "100%",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Text style={{ fontWeight: "bold", textAlign: "center" }}>
-                  Sign Up
-                </Text>
-              </TouchableOpacity>
-            </Link>
-          </View>
-          {/* </View> */}
-          <View style={{ flex: 1 }}>
-            <Link href="/reset">
-              <TouchableOpacity>
-                <Text style={{ fontWeight: "bold", marginTop: 10 }}>
-                  Forget Password
-                </Text>
-              </TouchableOpacity>
-            </Link>
-          </View>
+              <Text style={{ fontWeight: "bold", textAlign: "center" }}>
+                Sign Up
+              </Text>
+            </TouchableOpacity>
+          </Link>
         </View>
       </SignedOut>
     </LinearGradient>
   );
 }
+
+{
+  /* <View
+  style={{
+    justifyContent: "center",
+    alignItems: "center",
+    height: getScreenHeight() * 0.7,
+
+    width: getScreenWidth() * 0.7,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOpacity: 50,
+    shadowRadius: 3,
+    backgroundColor: thirdColor,
+    borderColor: "grey",
+  }}
+> */
+}
+//   <View
+//     style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+//   >
+//     <Link
+//       href="/sign-in"
+//       style={{
+//         height: "70%",
+//         width: "50%",
+//       }}
+//     >
+//       <TouchableOpacity
+//         style={{
+//           backgroundColor: fourthColor,
+//           borderRadius: 10,
+//           width: getScreenWidth() * 0.5,
+//           height: "100%",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <View
+//           style={{
+//             width: "100%",
+//           }}
+//         >
+//           <Text style={{ fontWeight: "bold", textAlign: "center" }}>
+//             Sign In
+//           </Text>
+//         </View>
+//       </TouchableOpacity>
+//     </Link>
+//   </View>
+//   <View style={{ flex: 1 }}>
+//     <Link
+//       href="/sign-up"
+//       style={{
+//         height: "70%",
+//         width: "50%",
+//       }}
+//     >
+//       <TouchableOpacity
+//         style={{
+//           backgroundColor: fourthColor,
+//           borderRadius: 10,
+//           width: getScreenWidth() * 0.5,
+//           height: "100%",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <Text style={{ fontWeight: "bold", textAlign: "center" }}>
+//           Sign Up
+//         </Text>
+//       </TouchableOpacity>
+//     </Link>
+//   </View>
+//   {/* </View> */}
+//   <View style={{ flex: 1 }}>
+// <Link href="/reset">
+//   <TouchableOpacity>
+//     <Text style={{ fontWeight: "bold", marginTop: 10 }}>
+//       Forget Password
+//     </Text>
+//   </TouchableOpacity>
+// </Link>
+//   </View>
+// </View>
